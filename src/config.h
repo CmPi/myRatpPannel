cbbbc17aa455f5768bdfd6e356798d132fa2eeac
cmpi <cmpi@free.fr>
2018-02-24 15:38:22 +0100

    First comit

---
+++ src/config.h
@@ -0,0 +1,8 @@
+
+#define OTA true
+
+#define TIME true
+
+#define TARGET_1 "BUS,131,Porte d'Italie,Les coquettes"    
+#define TARGET_2 "METRO,7,La Courneuve,Villejuif-Leo Lagrange" 
+  
---
+++ src/wifi.h
@@ -0,0 +1,4 @@
+//WIFI configuration
+#define wifi_ssid "my-ssid"
+#define wifi_password "my-password"
+#define WiFi_hostname "cmp_ratp"
